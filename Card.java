be31694e1c98a4e265ce1aae0b6074eb828d4a00
Ivan Neil <tapsivanneil@gmail.com>
2022-12-18 19:07:24 +0800

    Created getters and setters for the Card object

--- Card.java
+++ Card.java
@@ -1,3 +1,32 @@
 public class Card {
-    
+    private String cardName;
+    private double cardBalance;
+    private int idNum;
+
+    public void setCardName(String cardName){
+        this.cardName = cardName;
+    }
+
+    public void setCardBalance(double cardBalance){
+        this.cardBalance = cardBalance;
+    }
+
+    public void setIdNum (int idNum){
+        this.idNum = idNum;
+    }
+
+    public String getCardName(){
+        return cardName;
+    }
+
+    public double getCardBalance(){
+        return cardBalance;
+    }
+
+    public int getIdNum(){
+        return idNum;
+    }
+
+
 }
+
